5a0676d7865f4d8dceb023565c31a5835d5109e1
Leonid Shalagin <ll441369@gmail.com>
2021-09-11 16:21:36 +0300

    refactoring and removing using namespace std;

--- R-bonacci/main.cpp
+++ R-bonacci/main.cpp
@@ -10,35 +10,34 @@
 #include <string>
 #include <cstring>
 #include <complex>
+
 typedef long long ll;
 typedef std::vector<std::vector<ll> > matrix;
 
-using namespace std;
-
 ll MOD = 1000000009;
-int n,k;
+int n, k;
 matrix q;
 
-void print(const matrix& M) {
-    for(int i=0;i<k;i++){
-        for(int j=0;j<k;j++){
-            cout << M[i][j] << " ";
+void print(const matrix &M) {
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < k; j++) {
+            std::cout << M[i][j] << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
-    cout << endl;
+    std::cout << std::endl;
 }
 
-matrix matrixProduct(const matrix& A, const matrix& B) {
+matrix matrixProduct(const matrix &A, const matrix &B) {
     matrix C;
-    for(int i=0; i<k; i++){
-        C.push_back(vector<ll>(k));
-        for(int j=0; j<k; j++){
+    for (int i = 0; i < k; i++) {
+        C.push_back(std::vector<ll>(k));
+        for (int j = 0; j < k; j++) {
             C[i][j] = 0;
-            for(int z=0;z<k;z++){
+            for (int z = 0; z < k; z++) {
                 C[i][j] += (A[i][z] * B[z][j]) % MOD;
-                while(C[i][j] < 0) {
-                    C[i][j] = (C[i][j]+MOD)%MOD;
+                while (C[i][j] < 0) {
+                    C[i][j] = (C[i][j] + MOD) % MOD;
                 }
             }
         }
@@ -46,15 +45,15 @@ matrix matrixProduct(const matrix& A, const matrix& B) {
     return C;
 }
 
-matrix matrixPower(const matrix& M, int power) {
+matrix matrixPower(matrix &M, int power) {
     matrix result;
-    for(int i=0;i<k;i++){
-        result.push_back(vector<ll>(k,0));
+    for (int i = 0; i < k; i++) {
+        result.push_back(std::vector<ll>(k, 0));
         result[i][i] = 1;
     }
-    
-    while(power){
-        if(power&1){
+
+    while (power) {
+        if (power & 1) {
             result = matrixProduct(result, M);
         }
         M = matrixProduct(M, M);
@@ -63,34 +62,36 @@ matrix matrixPower(const matrix& M, int power) {
     return result;
 }
 
-int main()
-{
-    cout << "N-th element of R-bonacci sequence modulo MOD" << endl;
-    cout << "R: ";
-    cin >> k;
-    cout << "N: ";
-    cin >> n;
-    cout << "MOD: ";
-    cin >> MOD;
-    
-    for(int i=0;i<k;i++){
-        q.push_back(vector<ll>(k));
-        for(int j=0;j<k;j++){
+int main() {
+    std::cout << "N-th element of R-bonacci sequence modulo MOD" << std::endl;
+
+    std::cout << "R: " << std::flush;
+    std::cin >> k;
+
+    std::cout << "N: " << std::flush;
+    std::cin >> n;
+
+    std::cout << "MOD: " << std::flush;
+    std::cin >> MOD;
+
+    for (int i = 0; i < k; i++) {
+        q.push_back(std::vector<ll>(k));
+        for (int j = 0; j < k; j++) {
             q[i][j] = 0;
         }
     }
-    for(int i=0; i<k; i++){
+    for (int i = 0; i < k; i++) {
         q[i][0] = 1;
     }
-    for(int i=0; i<k; i++){
-        for(int j=1; j<k; j++){
-            if(j-1 == i){
+    for (int i = 0; i < k; i++) {
+        for (int j = 1; j < k; j++) {
+            if (j - 1 == i) {
                 q[i][j] = 1;
             }
         }
     }
-    
+
     auto answer = matrixPower(q, n);
-    cout << answer[0][0] << endl;
+    std::cout << answer[0][0] << std::endl;
     return 0;
 }
